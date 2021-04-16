f4dfe9e713504e9ff0f417439b456282b7890018
David Starr <david@elegantcode.com>
2021-04-16 10:30:13 -0700

    added unit test project

---
+++ src/LandingPageTest/HomeControllerTest.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace LandingPageTest
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Index()
+        {
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Privacy()
+        {
+            Assert.True(false);
+        }
+    }
+}
