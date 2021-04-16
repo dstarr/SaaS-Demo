5bd3f358fb2a80588b213f91f3048d5b8a825ba6
David Starr <david@elegantcode.com>
2021-04-16 11:12:46 -0700

    added Index() test with User

--- src/LandingPageTest/HomeControllerTest.cs
+++ src/LandingPageTest/HomeControllerTest.cs
@@ -1,20 +1,50 @@
-using System;
 using Xunit;
+using src.Controllers;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Security.Principal;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace LandingPageTest
 {
     public class HomeControllerTest
     {
+        HomeController _controller;
+        ControllerContext _controllerContext;
+
+        public HomeControllerTest()
+        {
+            _controller = new HomeController(new NullLogger<HomeController>());
+
+            _controllerContext = _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext
+                {
+                    User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim(ClaimTypes.Name, "username")
+                    }, "someAuthTypeName"))
+                }
+            };
+        }
+
+
         [Fact]
         public void Index()
         {
-            Assert.True(true);
+            IActionResult result = _controller.Index();
+
+            Assert.NotNull(result);
         }
 
         [Fact]
         public void Privacy()
         {
-            Assert.True(false);
+            IActionResult result = _controller.Privacy();
+
+            Assert.NotNull(result);
         }
     }
 }
