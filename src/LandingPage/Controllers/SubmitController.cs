1ff9b4b5a83766218131231dea1ec5b919089d06
David Starr <david@elegantcode.com>
2021-10-05 12:56:32 -0700

    Added submission page

---
+++ src/LandingPage/Controllers/SubmitController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LandingPage.Controllers
+{
+    public class SubmitController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
